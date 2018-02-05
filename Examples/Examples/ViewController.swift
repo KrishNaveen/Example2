556aebf21ba8401f1d9ab9c359e4b1bf83f635f5
Naveenkrishna Manda <naveen.manda@invigilus.com>
2018-02-05 14:07:28 +0530

    Test1

--- Examples/Examples/ViewController.swift
+++ Examples/Examples/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Test Branch 1 committing")
+        print("Test Branch 1 committing Dummy")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
